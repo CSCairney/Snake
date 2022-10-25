6caa4c347d19c9ccf2eb3811a1c4006b81840745
CSCairney <charlescairney@yahoo.co.uk>
2022-10-25 12:33:25 +0100

    Working functional application

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,6 +2,11 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3868855034012153296L;
+
 	GameFrame(){
 				
 		this.add(new GamePanel());
